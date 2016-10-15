8e2285bc29accb85b16c23a756283ce441a1235b
rpmartinsen <rpmartinsen@ucdavis.edu>
2016-10-15 11:54:57 -0700

    added Procfile

--- index.py
+++ index.py
@@ -42,4 +42,5 @@ api.add_resource(Conversation, '/convo')
 api.add_resource(Sentence, '/sentence/<person_name>')
 
 if __name__ == '__main__':
-	app.run(debug = True, port=5001)
+	port = int(os.environ.get("PORT", 5000))
+	app.run(host='0.0.0.0', debug = True, port=port)
