959fac198ff2eb027ca446a01e909077b219b861
rpmartinsen <rpmartinsen@ucdavis.edu>
2017-12-13 11:10:20 -0500

    Refactored resources

--- app/markov_maker.py
+++ app/markov_maker.py
@@ -1,6 +1,5 @@
 import random
 import re
-from corpora import corpora
 
 MIN_NGRAMS_IN_CHAIN = 8
 MAX_NGRAMS_IN_CHAIN = 18
@@ -8,25 +7,24 @@ MAX_NGRAMS_IN_CHAIN = 18
 
 class MarkovMaker():
 
-    def __init__(self, target_name, corpora=corpora):
+    def __init__(self, corpora, person_name):
         """
         Initialize MarkovMaker with name as it appears in corpora. A random chain can then be
-        created using the create_chain method.
+        created using the create_sentence method.
 
-        target_name: Name exactly as it appears in corpora key If name does not exist in corpora
+        person_name: Name exactly as it appears in corpora key If name does not exist in corpora
                      an error will be thrown.
         """
-        self.target_name = target_name
-        self.corpus = corpora[target_name]
+        self.person_name = person_name
+        self.corpus = corpora[person_name]
         self.tokenized_corpus = self._tokenize_corpus()
         self.triples = self._get_triples()
 
-    def create_chain(self):
+    def create_sentence(self):
         """
         Creates a random markov chain based on n_grams of length 3. Warning: The accuracy
         of how closely it imitates people can be spooky!
         """
-        print(self.tokenized_corpus)
         length = self._get_random_chain_length()
         word = random.choice(self.tokenized_corpus)
         chain = [word]
@@ -73,3 +71,40 @@ class MarkovMaker():
                 candidates.append(ngram)
 
         return candidates
+
+
+def create_conversation(corpora, min_sentences=4, max_sentences=16):
+    """
+    Creates conversation of random sentences based on corpora.
+
+        Arguments:
+        corpora: A dictionary-like object where the key is the person name and the value is the
+                 corpus used to
+        min_sentences: Smallest number of sentences in random conversation length.
+        max_sentences: Largest number of sentences in random conversation length.
+    """
+    people = []
+
+    for person in corpora.keys():
+        people.append(person)
+
+    convo = []
+
+    num_sentences = random.randint(min_sentences, max_sentences)
+
+    for i in range(num_sentences):
+        person_name = random.choice(people)
+        sentence = create_sentence(corpora, person_name)
+        convo.append(sentence)
+
+    return convo
+
+
+def create_sentence(corpora, person_name):
+    if not person_name:
+        person_name = corpora.keys()[0]
+
+    mm = MarkovMaker(corpora, person_name)
+    sentence = mm.create_sentence()
+    return {"name": person_name,
+            "sentence": sentence}
--- app/resources.py
+++ app/resources.py
@@ -1,35 +1,38 @@
-import random
-from collections import namedtuple
 from flask_restful import Resource
 
-from app.markov_maker import MarkovMaker
+from app.markov_maker import create_sentence, create_conversation
+from default_corpora import corpora
+
+MIN_CONVO_SENTENCES = 4
+MAX_CONVO_SENTENCES = 16
 
 
 class Conversation(Resource):
-    def get(self, corpora):
+    def get(self, corpora=corpora,
+            min_convo_sentences=MIN_CONVO_SENTENCES,
+            max_convo_sentences=MAX_CONVO_SENTENCES):
         """
-        Returns one sent
+        Flask-restful resource to create conversation of random sentences based on a corpora.
+
+        Arguments:
+        corpora: A dictionary-like object where the key is the person name and the value is the
+                 corpus used to
+        min_convo_sentences: Smallest number of sentences in random conversation length.
+        max_convo_sentences: Largest number of sentences in random conversation length.
         """
-        Sentence = namedtuple("sentence", "name sentence")
-
-        folks = []
-
-        for corpus in corpora:
-            mm = MarkovMaker(corpus)
-            folks.append(mm)
-
-        convo = []
-        for i in range(random.randint(4, 16)):
-            person = random.choice(folks)
-            sentence = Sentence(person.target_name, person.create_chain())
-            convo.append(sentence._asdict())
-
-        return(convo)
+        conversation = create_conversation(corpora, MIN_CONVO_SENTENCES, MAX_CONVO_SENTENCES)
+        return(conversation)
 
 
 class Sentence(Resource):
-    def get(self, person_name=None):
-        if not person_name:
-            person_name = "Derek"
-        mm = MarkovMaker(person_name)
-        return("<strong>" + person_name + "</strong>" + ": " + mm.create_chain())
+    def get(self, corpora=corpora, person_name=None):
+        """
+        Flask-restful resource to create sentence for single user.
+
+        Arguments:
+        corpora: Dictionary-like object where keys are person names and values are the corpus to be
+                 used
+        person_name: person_name matching one of the keys in corpora.
+        """
+        sentence = create_sentence(corpora, person_name)
+        return sentence
